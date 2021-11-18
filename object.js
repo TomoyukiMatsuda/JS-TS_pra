aeb4a375efc26cfa70dd33acde3d569ea1b2176f
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-11-18 09:29:58 +0900

    アッパーオブジェクト

--- object.js
+++ object.js
@@ -1,5 +1,12 @@
 // ビルトインオブジェクト
+// jsエンジンに標準で用意されているオブジェクト群
 const arry = new Array(1, 2, 3, 4)
 console.log(arry)
 
 // ラッパーオブジェクト
+const str = new String('hello')
+// const str = 'hello' // これでも同じこと
+console.log(str.toUpperCase())
+
+const n = new Number(100)
+console.log(n.toExponential())
