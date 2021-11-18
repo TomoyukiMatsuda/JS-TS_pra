280013d86ef7f6217a252b3a9d763eeac0b8f9d5
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-11-18 09:42:56 +0900

    シンボル( Symbol

--- object.js
+++ object.js
@@ -10,3 +10,19 @@ console.log(str.toUpperCase())
 
 const n = new Number(100)
 console.log(n.toExponential())
+
+// シンボル Symbol
+const s = Symbol('hello')
+const s2 = Symbol('hello')
+// 異なる値が生成されるので上記2つを比較してもfalseになる
+console.log(s, s2, s === s2, typeof s) // Symbol(hello) Symbol(hello) false 'symbol'
+
+const st = new String('Tom')
+console.log(st)
+
+// 基本的にこのような記述はしない
+String.prototype[s] = function() {
+  return 'hello ' + this
+}
+const tom = 'Tom'
+console.log(tom[s]()) // hello Tom
