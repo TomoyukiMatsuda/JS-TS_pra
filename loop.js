a5825a20f7e45696df10f273acfa43ef93ce66b7
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-11-19 09:27:37 +0900

    演算子

--- loop.js
+++ loop.js
@@ -1,3 +1,4 @@
+//反復処理
 for(let i = 0; i < 10; i++) {
   console.log(i)
 }
@@ -7,3 +8,22 @@ while(i < 10) {
   console.log(i)
   i++
 }
+
+// 演算子
+let a = 0
+let b = ++a // aに++した後の結果をreturnする
+// a = b = 1 を行っている
+console.log(`a: ${a}, b: ${b}`) // a: 1, b: 1
+
+let c = 0
+let d = 0
+//let d = c++ // cに++する前の結果をreturnする
+// c = (d = c) + 1 を行っている
+console.log(`c: ${c}, d: ${d}`) // c: 1, d: 0
+
+function fn() {
+  let f = 0
+  return f++ // 0がreturnされる
+}
+// !fn()で0がtrueになる、true*5で数値型変換がされて1*5となる
+console.log(!fn()*5) // 5
