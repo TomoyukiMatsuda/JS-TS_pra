56db6b4c18a9e1eb1f443d208ced894e29914161
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-11-21 12:32:14 +0900

    イテレーターとスプレッド演算子

--- spread_operator.js
+++ spread_operator.js
@@ -16,3 +16,18 @@ function sum(...args) {
 }
 const result = sum(1, 2, 3, 4)
 console.log(result)
+
+const obj = {
+  prop1: 'val1',
+  prop2: 'val2',
+  prop3: 'val3',
+}
+// これでGenerator(iterator)がobj（Objectに設定される）
+Object.prototype[Symbol.iterator] = function*() {
+  for(let key in this) {
+    yield [key, this[key]]
+  }
+}
+// 上記によりiteratorが機能するため[...obj]が動作する
+const arry3 = [...obj]
+console.log(arry3)
