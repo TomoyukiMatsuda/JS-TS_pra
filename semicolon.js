1586109004ea568d35ece22369b91bda476172ab
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-11-22 12:59:03 +0900

    セミコロン有無の考察

--- semicolon.js
+++ semicolon.js
@@ -14,3 +14,25 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', function() {
   console.log('ボタンクリック')
 })
+
+function foo() {
+  let point = {}
+//  const lat = 35
+//  const lng = 132
+
+  try {
+    // tryスコープの中で宣言するとエラーになる
+    // VM50:8 ReferenceError: Cannot access 'lng' before initialization
+    // at foo (<anonymous>:6:36)
+    // at <anonymous>:11:1
+    const lat = 35
+    const lng = 132
+
+    //[point.lat, point.lng] = [lat, lng]
+    point = {lat, lng}
+  } catch (e) {
+    console.log(e)
+  }
+  console.log(point)
+}
+foo()
