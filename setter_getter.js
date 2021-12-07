7eaceb32bda7cba1f1f1b5d8e2de61ddf9650e40
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-12-07 10:23:03 +0900

    getterとsetterとstatic

---
+++ setter_getter.js
@@ -0,0 +1,65 @@
+function Person(name, age) {
+  this._name = name
+  this._age = age
+}
+// 関数コンストラクタの場合にgetterとsetterを設定
+Object.defineProperty(Person.prototype, 'name', {
+  get: function () {
+    console.log('get:', this._name)
+    return this._name
+  },
+  set: function (val) {
+    console.log('set:', val)
+    this._name = val
+  }
+})
+// 関数コンストラクタのstaticメソッド
+Person.hello = function () {
+  console.log('ハロー、パーソン１')
+}
+Person.hello()
+
+const p1 = new Person('パーソン１', 22)
+console.log(p1)
+console.log(p1.name)
+p1.name = '名前を変更したい'
+
+console.log('############### class でgetterとsetter ##############')
+// classでgetterとsetterを設定
+class Person2 {
+  constructor(name, age) {
+    this._name = name
+    this._age = age
+  }
+
+  // nameプロパティのgetterとsetter
+  get name() {
+    console.log('get:', this._name)
+    return this._name
+  }
+  set name(val) {
+    console.log('set:', val)
+    this._name = val
+  }
+
+  // ageプロパティのgetterとsetter
+  get age() {
+    console.log('get:', this._age)
+    return this._name
+  }
+  set age(val) {
+    console.log('set:', val)
+    this._age = val
+  }
+
+  static hello() {
+    // staticメソッドはインスタンスから呼ばれる訳ではないため、thisにはアクセスできない
+    console.log('this: ', this) // class Person2 自信が出力されてしまう
+    console.log('ハローーーー')
+  }
+}
+
+const p2 = new Person2('パーソン2', 29)
+console.log(p2.age)
+p2.age = 33
+Person2.hello()
