5ef2ebc917c5dd61624cb0c48ef74b9005c475a0
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-12-11 12:13:10 +0900

    型推論 widening

--- ts/shimabuu_ts/pages/index.tsx
+++ ts/shimabuu_ts/pages/index.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from "next";
 
+// const と let では型推論の挙動に違いがある
+const foo = "foo"; // "foo"
+let bar = "bar"; // string
+
+let foo2 = foo; // string wideningされちゃう
+// wideningを防ぐ方法３つ
+// const foo: "foo" = "foo"; // "foo"
+// const foo = "foo" as "foo"; // "foo"
+// const foo = "foo" as const; // "foo"
+
 const Home: NextPage = () => {
-  return <div>テスト</div>;
+  return <Component foo />;
+};
+
+const Component = (props: { foo?: true }) => {
+  if (props.foo) {
+    return <div>true</div>;
+  }
+  return <div>false</div>;
 };
 
 export default Home;
