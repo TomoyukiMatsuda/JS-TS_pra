64473e04f0384449b395c0037cefd7602767d122
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-12-11 14:16:14 +0900

    さまざまな型 void never unknown any

--- ts/shimabuu_ts/pages/index.tsx
+++ ts/shimabuu_ts/pages/index.tsx
@@ -10,6 +10,31 @@ let foo2 = foo; // string wideningされちゃう
 // const foo = "foo" as "foo"; // "foo"
 // const foo = "foo" as const; // "foo"
 
+// Tuple
+const foo3: [string, number, string] = ["str", 200, "str"];
+// unknown は 利用する時に型が定義されていないことを教えてくれる、エラーが出る（利用する時に型安全）
+const foo4: unknown = "foo";
+foo.slice(2, 2);
+// foo4.slice(3, 2) // エラー
+
+// void
+const func = (): void => {
+  alert("ハロー");
+  return undefined; // void = return undefined と同じなためエラーにならない
+};
+
+// never ありえない値
+const foo = (bar: "a" | "b") => {
+  switch (bar) {
+    case "a":
+      return 1;
+    case "b":
+      return 2;
+    default:
+      bar; // never型 defaultに到達するはずないので never型になる
+  }
+};
+
 const Home: NextPage = () => {
   return <Component foo />;
 };
