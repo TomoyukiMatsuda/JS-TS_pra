2f0eba886a5010cf4817ccd9683bb17fcf2f02c7
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-12-20 10:31:55 +0900

    forEach, map, filter, reduce

--- js/array.js
+++ js/array.js
@@ -18,3 +18,26 @@ console.log(arry)
 const arr2 = [10, 11, 12, 13, 14]
 const arr3 = arry.concat(arr2)
 console.log(arr3)
+
+// forEach((value, index, array)=>{})
+const arr4 = [1, 2, 3, 4, 5]
+arr4.forEach((value, index, array) => console.log(value, index, array))
+
+const newArray = arr4.map((v, i, a) => {
+  console.log(v, i, a)
+  return a
+})
+console.log('newArray', newArray)
+
+const filterArry = arr4.filter((v, i, a) => {
+  // truthyな値の場合に返す
+  return v % 2 === 0
+})
+console.log(filterArry)
+
+console.log('######### reduce ############')
+const result = arr4.reduce((accuVal, currVal) => {
+  console.log(accuVal, currVal)
+  return accuVal + currVal
+}, 10) // initialValue 初期値（省略した場合は配列の先頭が初期値となる）
+console.log('result', result)
