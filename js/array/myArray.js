d8282da6ff0f59067c11e51fec2048dcf02d8ecd
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-12-25 08:05:52 +0900

    Array関数を自作してみるpush

---
+++ js/array/myArray.js
@@ -0,0 +1,50 @@
+/**
+ * 問題：
+ * Arrayを継承して以下のメソッドを実装してみましょう。
+ *
+ * push(*1)
+ * forEach
+ * map
+ * filter
+ * reduce
+ *
+ * *1:pushはチェーンメソッドとしてつなげられるように実装してみてください。
+ */
+class MyArray extends Array {
+  constructor(...args) {
+    super(...args)
+  }
+
+  print(label = '') {
+    console.log(`%c ${label}`, 'color: blue; font-weight: 600;', this);
+    return this;
+  }
+  push(item) {
+    return [...this, item]
+  }
+  forEach(callbackfn, thisArg) {
+    
+  }
+}
+
+function double(v, i, obj) {
+  return v * 2;
+}
+
+const original = new MyArray(1, 2, 3, 4);
+
+console.log(original.push(5))
+
+// const result = original
+//   .map(double)
+//   .push(5)
+//   .filter(function (v, i) {
+//     return v > 2;
+//   })
+//   .reduce(function(accu, curr) {
+//     return accu + curr;
+//   })
+//
+// console.log('%coriginal', 'color: blue; font-weight: bold;', original);
+// console.log('%cresult', 'color: red; font-weight: bold;', result);
+//
