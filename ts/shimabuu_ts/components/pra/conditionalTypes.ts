ea904ffd0b4e3607a75755896e137851e4d803da
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2022-05-02 07:03:53 +0900

    infer: 部分的な型抽出の応用

--- ts/shimabuu_ts/components/pra/conditionalTypes.ts
+++ ts/shimabuu_ts/components/pra/conditionalTypes.ts
@@ -15,7 +15,11 @@ const sk: StringKeys = "name";
 type NumberKeys = Filter<Props, number>;
 const nk: NumberKeys = "age";
 
-const foo = () => "";
-// infer: 部分的な型抽出, これは部分的な関数の返り値の型を抽出している
-type Return<T> = T extends () => infer U ? U : never;
+const foo = (id: number, age: string, isMan: boolean) => "";
+// infer: 部分的な型抽出, これは部分的に関数の返り値の型を抽出している
+type Return<T> = T extends (...args: any) => infer U ? U : never;
 type FooType = Return<typeof foo>; // stringが取れる
+type ArgsType<T> = T extends (...args: [any, any, infer U, ...any[]]) => string
+  ? U
+  : never;
+type FooArgsType = ArgsType<typeof foo>;
